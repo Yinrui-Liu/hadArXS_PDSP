c60db416fe38a5164edcb2b8c04c36ab8956c005
jrositas <jrositas@uchicago.edu>
2025-04-15 15:57:30 -0500

    Fixed further issues with calc_trueXS. Can probably make the code run faster by flattening the true_Eini and similar arrays rather than appending as I am currently, but I have verified that this version produces results consistent with the original XS results, provided it is being run with the old version of slicing_method.

--- calc_trueXS.py
+++ calc_trueXS.py
@@ -1,26 +1,24 @@
 from hadana.packages import *
 import hadana.slicing_method as slicing
 from hadana.BetheBloch import BetheBloch
-from hadana.multiD_mapping import get_SID2Dmap
 
 selected_type = "prod" # Sets the type of interaction being analyzed.
 conversion_dict = {"abs": 4, "cex": 2, "dcex": 1,
               "inel": 1, "prod": 5, "incl": -1}
 type_int = conversion_dict[selected_type]
 beamPDG = 211
-file_name_prefix = "processed_files/procVars_piMC_test" # TODO Deprecated, remove at earliest convinience.
+file_name_prefix = "processed_files/procVars_piMC_" # TODO Deprecated, remove at earliest convinience.
 file_name_suffix = ".pkl"
 if selected_type == "incl": file_name = "processed_files/procVars_piMC" + file_name_suffix
 else: file_name = file_name_prefix + selected_type + file_name_suffix
-file_name = "processed_files/procVars_piMC_test2.pkl"
+file_name = "processed_files/procVars_piMC.pkl"
 with open(file_name, 'rb') as procfile:
     processedVars = pickle.load(procfile)
 if beamPDG == 211:
     true_bins = np.array([1000,950,900,850,800,750,700,650,600,550,500,450,400,350,300,250,200,150,100,50,0])
 elif beamPDG == 2212:
     true_bins = np.array([500,475,450,425,400,375,350,325,300,275,250,225,200,175,150,125,100,75,50,25,0])
-Ntrueinttype = 6 # number of true interaction types ["noint", "inel", "cex", "dcex", "abs", "prod"]
-signal_int_type = 1 # same as the selected_type
+
 
 mask_TrueSignal = processedVars["mask_TrueSignal"]
 # type_mask = [int_type == type_int for int_type in processedVars["true_int_type"]]
@@ -50,7 +48,7 @@ true_Eend = np.array([])
 true_flag = np.array([])
 true_isCt = np.array([])
 true_weight = np.array([])
-for interaction_type in range(1, 6):
+for interaction_type in range(0, 14):
     true_Eini = np.append(true_Eini, divided_trueEini[interaction_type])
     true_Eend = np.append(true_Eend, divided_trueEend[interaction_type])
     true_flag = np.append(true_flag, divided_trueflag[interaction_type])
@@ -64,10 +62,8 @@ true_SIDini, true_SIDend, true_SIDint_ex = slicing.get_sliceID_histograms(true_E
 #here goes the exclusive selections
 
 true_Nini, true_Nend, true_Nint_ex, true_Ninc = slicing.derive_energy_histograms(true_SIDini, true_SIDend, true_SIDint_ex, Ntruebins, true_weight)
-SID2Dmap, Ntruebins_2D = get_SID2Dmap(Ntruebins)
-Ntruebins_3D = Ntruebins_2D * Ntrueinttype # number of bins for the combined 3D variable (IDini, IDend, int_type)
-true_SID3D, true_N3D, true_N3D_Vcov = slicing.get_3D_histogram(true_SIDini, true_SIDend, true_flag, Ntruebins_2D, Ntruebins_3D, true_weight)
-true_3SID_Vcov = slicing.get_Cov_3SID_from_N3D(true_N3D_Vcov, Ntruebins, Ntruebins_2D, Ntruebins_3D, SID2Dmap, signal_int_type) # here signal_int_type should be an integer. TB considered to enable the case of inclusive.
+true_SID3D, true_N3D, true_N3D_Vcov = slicing.get_3D_histogram(true_SIDini, true_SIDend, true_SIDint_ex, Ntruebins, true_weight)
+true_3SID_Vcov = slicing.get_Cov_3SID_from_N3D(true_N3D_Vcov, Ntruebins)
 true_3N_Vcov = slicing.get_Cov_3N_from_3SID(true_3SID_Vcov, Ntruebins)
 true_XS, true_XS_Vcov = slicing.calculate_XS_Cov_from_3N(true_Ninc, true_Nend, true_Nint_ex, true_3N_Vcov, true_bins, BetheBloch(beamPDG))
 print("true_XS:", true_XS)
